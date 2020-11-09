5208205b821d151a077d3ce4bcdf99e3a23056a6
Yasmine091 <yasmine.emdounan@gmail.com>
2020-11-09 14:53:47 +0100

    Exercices parametres - base

---
+++ parametres.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Intro PHP #2 - Parametres</title>
+</head>
+<body>
+    <?php
+    # Afficher les erreurs
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+
+    
+    ?>
+</body>
+</html>
