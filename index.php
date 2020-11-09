91a1231e2143be40f056614554997c15c6d38509
Yasmine091 <yasmine.emdounan@gmail.com>
2020-11-09 11:54:22 +0100

    Exercice 1

--- index.php
+++ index.php
@@ -7,27 +7,14 @@
 </head>
 <body>
 
-<!-- Consigne 1 -->
+<!-- Exercice 1 -->
 
 <?php
     for ($i = 0; $i <= 10; $i++) {
     echo $i . "<br>";
 }
 ?> 
-<hr>
 
-<!-- Consigne 2 -->
-<form method="POST">
-<?php
-    for ($a = 0; $a <= 20; $a++) {
-    $b = mt_rand(0, 101);
-    print '<input type="hidden" name="rnd-b" value="'. $b .'">';
-    $nb = $_POST['rnd-b'];
-    $multp = $a * $nb;
-    echo $multp . "<br>";
-}
-?>
-</form>
 
 </body>
 </html>
